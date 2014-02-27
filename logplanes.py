279c0a6756305c2780f8d870afba2a587b10cd4d
Chuck Swiger <cswiger@swigerco.com>
2014-02-27 18:48:17 -0500

    added python file

---
+++ logplanes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import MySQLdb, os, datetime
+
+mysql = MySQLdb.connect(host='192.168.1.119',user='aircraft',passwd='Password01',db='aircraft')
+mysqlcursor = mysql.cursor()
+
+adsb = os.popen("/home/chuck/src/dump1090/dump1090 --net --aggressive")
+
+while True:
+   line = adsb.readline()
+   if ( line.find('    Identification ') != -1) :
+     now = datetime.datetime.today()
+     timestamp = str(now.year) + '-' + str(now.month) + '-' + str(now.day) + ' ' + str(now.hour) + ':' + str(now.minute) + ':' + str(now.second)
+     dummy,cs = line.split(':')
+     callsign = cs.strip()
+     sql = "select id from aircraft where ident = '" + callsign + "'"
+     if ( mysqlcursor.execute(sql) == 0L ):
+	sql = "insert into aircraft values(NULL, '" + callsign + "', '" + timestamp + "')"
+        mysqlcursor.execute(sql)
+
