ce193f54f3fef6b22794ab2885c1e1eaa84d7278
Chuck Swiger <cswiger@swigerco.com>
2014-02-28 15:36:06 -0500

    Changed sql to check for existance of an aircraft only within the last hour, so we can log other times they pass over.

--- logplanes.py
+++ logplanes.py
@@ -11,10 +11,12 @@ while True:
    line = adsb.readline()
    if ( line.find('    Identification ') != -1) :
      now = datetime.datetime.today()
+     hrago = now + datetime.timedelta(0,0,0,0,0,-1,0)     # adding -1 hour
      timestamp = str(now.year) + '-' + str(now.month) + '-' + str(now.day) + ' ' + str(now.hour) + ':' + str(now.minute) + ':' + str(now.second)
+     hrago_timestamp = str(hrago.year) + '-' + str(hrago.month) + '-' + str(hrago.day) + ' ' + str(hrago.hour) + ':' + str(hrago.minute) + ':' + str(hrago.second)
      dummy,cs = line.split(':')
      callsign = cs.strip()
-     sql = "select id from aircraft where ident = '" + callsign + "'"
+     sql = "select id from aircraft where ident = '" + callsign + "' and spotted > '" + hrago_timestamp + "'"
      if ( mysqlcursor.execute(sql) == 0L ):
 	sql = "insert into aircraft values(NULL, '" + callsign + "', '" + timestamp + "')"
         mysqlcursor.execute(sql)
